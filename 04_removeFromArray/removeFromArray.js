0818a6b922a3c5604ee0a1b073da2fd8714a320d
PonyPaucity <ivanang109@gmail.com>
2022-02-02 14:21:13 +0800

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...removedElements) {
+    for (let i = 0; i < inputArray.length; i++) {
+        for (let j = 0; j < removedElements.length; j++) {
+            if (inputArray[i] === removedElements[j]) {
+                console.log(inputArray[i]);
+                inputArray.splice(i, 1)
+                i = 0;
+            }
+        }
+    } return inputArray
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
