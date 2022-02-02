fe7696d186d9a486ca827ef363e5a82443bb35bc
PonyPaucity <ivanang109@gmail.com>
2022-02-02 14:47:21 +0800

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (initialNumber, finalNumber) {
+    let sum = 0;
 
+    if (initialNumber < 0 || finalNumber < 0) {
+        return 'ERROR'
+    } else if (typeof initialNumber === 'string' || typeof finalNumber === 'string') {
+        return 'ERROR'
+    } else if (typeof initialNumber === 'object' || typeof finalNumber === 'object') {
+        return 'ERROR'
+    } else if (finalNumber > initialNumber) {
+        for (let i = initialNumber; i < finalNumber + 1; i++) {
+            sum += i
+        } return sum
+    } else if (finalNumber < initialNumber) {
+        for (let i = finalNumber; i < initialNumber + 1; i++) {
+            sum += i
+        } return sum
+    } 
 };
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
