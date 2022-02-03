64aea187ff86cc4c5d28db6117a033fe987ab36a
PonyPaucity <ivanang109@gmail.com>
2022-02-03 17:08:05 +0800

    completed exercises 7, 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function (a) {
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i]
+  } return sum
 };
 
-const multiply = function() {
-
+const multiply = function (a) {
+  let multiplcation = 1;
+  for (let i = 0; i < a.length; i++) {
+    multiplcation *= a[i]
+  } return multiplcation
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  return n ? n * factorial(n - 1) : 1;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+function checkPalindrome(string) {
+    if (string.length % 2 === 0) {
+        for (let i = 0; i < string.length/2; i++){
+            if (string[i] === string[string.length - 1 - i]){
+                continue
+            } else return false;
+        } return true
+    } else {
+        let halfStringLength = Math.round(string.length/2) - 1
+        for (let i = 0; i < halfStringLength; i++){
+            if (string[i] === string[string.length - 1 - i]){
+                continue
+            } else return false;
+        } return true
+    }
+}
 
+const palindromes = function (string) {
+    string = string.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '');
+    string = string.replace(/\s+/g, '');
+    string = string.toLowerCase();
+    return checkPalindrome(string);
 };
+
+palindromes('Animal loots foliated detail of stool lamina.');
 
 // Do not edit below this line
 module.exports = palindromes;
